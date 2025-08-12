020500b9ddad53b71fa8a58aaabb90c5130deb3a
ArshadMQ <arshad.qureshi@cloudanalogy.com>
2025-08-12 13:11:49 +0530

    added requirements

--- connections/db_connection.py
+++ connections/db_connection.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Load from env or hardcoded fallback
-DB_USER = os.getenv("POSTGRES_USER", "neondb")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "riskcore_db")
-DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_USER = os.getenv("POSTGRES_USER", "neondb_owner")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "npg_7Yb1tpICxvdQ")
+DB_HOST = os.getenv("POSTGRES_HOST", "ep-damp-dream-a1fv7xe6-pooler.ap-southeast-1.aws.neon.tech")
 DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB", "riskcore_db")
+DB_NAME = os.getenv("POSTGRES_DB", "neondb")
 # psql 'postgresql://neondb_owner:npg_7Yb1tpICxvdQ@ep-damp-dream-a1fv7xe6-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
